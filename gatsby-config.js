8fbac76820b814a3b085c780c9126b88a1a2a07a
gercamjr <gercamjr.dev@gmail.com>
2022-06-29 10:51:50 -0500

    initial project creation

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,24 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-mysql`,
+      options: {
+        connectionDetails: {
+          host: 'legacyrecorder.org',
+          user: 'legacder_dev',
+          password: '55841921',
+          database: 'legacder_gerardotest'
+        },
+        queries: [
+          {
+            statement: 'Select * from audio_recordings',
+            idFieldName: 'id',
+            name: 'audio_recordings'
+          }
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
